353a2101a79e16fa62e3e41f4fcd32c359c9b33c
khorasany <a.khorasany@gmail.com>
2022-08-14 03:09:38 +0430

    add 2 migration & models

--- src/database/migrations/2022_08_13_222846_create_challenges_table.php
+++ src/database/migrations/2022_08_13_222846_create_challenges_table.php
@@ -16,6 +16,7 @@ class CreateChallengesTable extends Migration
         Schema::create('challenges', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
         });
     }
 
